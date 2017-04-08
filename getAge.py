9c585bac82c6acea69e7d17b79102bf5e52a0a2c
Trevor Darley <trevor.darley6.2@gmail.com>
2017-04-08 11:25:59 -0400

    Created getAge and added more to variables.py

---
+++ getAge.py
@@ -0,0 +1,7 @@
+# @author: Trevor Darley
+# @Description : Ask for name and age, then print them
+print "What is your name?"
+name = raw_input()
+print "How old are you?"
+age = raw_input()
+print "%s is %s years old" % (name,age)
--- variables.py
+++ variables.py
@@ -1,2 +1,6 @@
 age = 16    #Defining age
-print "I am ", age  #print age
+num1 = 5
+num2 = 6
+print "I am %d years old" %age  #print age in formatted format
+print "If I add %d, %d and %d I get %d" % (age, num1, num2, age + num1 + num2) #adds numbers
+print "\nHello \t\t world\n" #Tabbed Hello world
