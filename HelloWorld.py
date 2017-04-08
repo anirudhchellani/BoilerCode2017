5e1e57f8a7df60fdbdafc932a759b29c2c557ab5
Anirudh Manohar Chellani <anirudhchellani@gmail.com>
2017-04-08 00:31:56 -0400

    Hello World Program

---
+++ HelloWorld.py
@@ -0,0 +1,6 @@
+# @author Anirudh Manohar Chellani
+# @Description Hello World Program for Boiler Code 2017
+
+print "\nHello World!\n"	# Prints Hello World on the terminal screen
+							#\n Prints New Line
+
---
+++ HelloWorld_Recommended.py
@@ -0,0 +1,9 @@
+# @author Anirudh Manohar Chellani
+# @Description Hello World Program for Boiler Code 2017
+
+def main():						# The Function that is invoked by the terminal whenever a python file is run
+	print "\nHello World!\n"	# Prints Hello World on the terminal screen
+								# \n Prints New Line on the Scren
+
+if __name__ == '__main__':		# Checks if the file is being executed or used as a modukle
+	main()						# Executes the main function if the file is being executed
