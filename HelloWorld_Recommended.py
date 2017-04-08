7d1c94b153d2b947b28df2b9ab6700772a9ec07a
Trevor Darley <trevor.darley6.2@gmail.com>
2017-04-08 11:03:22 -0400

    Added 'name' function

--- HelloWorld_Recommended.py
+++ HelloWorld_Recommended.py
@@ -1,9 +1,13 @@
 # @author Anirudh Manohar Chellani
+# @author Trevor Darley
 # @Description Hello World Program for Boiler Code 2017
 
-def main():						# The Function that is invoked by the terminal whenever a python file is run
-	print "\nHello World!\n"	# Prints Hello World on the terminal screen
+def printHelloWorld():						# The Function that is invoked by the terminal whenever a python file is run
+    print "\nHello World!\n"	# Prints Hello World on the terminal screen
 								# \n Prints New Line on the Scren
 
+def name():
+    print "Trevor\n"
+
 if __name__ == '__main__':		# Checks if the file is being executed or used as a modukle
-	main()						# Executes the main function if the file is being executed
+	name()					# Executes the main function if the file is being executed
