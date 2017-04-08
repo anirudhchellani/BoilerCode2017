19f030a3b3f1475ee4dcb9f8b10e63a75a956f67
Temporary Account <guest11@sslab17.cs.purdue.edu>
2017-04-08 10:59:36 -0400

    Added more messages to HelloWorld

--- HelloWorld.py
+++ HelloWorld.py
@@ -1,6 +1,11 @@
 # @author Anirudh Manohar Chellani
+# @author Trevor Darley
 # @Description Hello World Program for Boiler Code 2017
 
 print "\nHello World!\n"	# Prints Hello World on the terminal screen
-							#\n Prints New Line
+				# \n Prints New Line
+print "Hello there!"
+
+print "I'm Trevor\n"
+
 
